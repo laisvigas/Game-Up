4f1b746d3a0f03d523db81721519de67ece86a4c
DanielCaseiro <daniel.caseiro@gmail.com>
2025-05-09 13:29:24 +0100

    modal para expandir as fotos na galeria; e as divs com as classes no Index agora são clickables e abrem uma nova aba no navegador com a respectiva classe na página treinos

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,32 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+// js para ver as imagens da galeria ampliadas ao clicar nelas (utilizando tb o modal do bootstrap)
+
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryItems = document.querySelectorAll('.image-gallery-item');
+    const imageModalElement = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    let imageModalInstance; // To store the Bootstrap Modal instance
+
+    imageModalElement.addEventListener('show.bs.modal', function(event) {
+        const clickedGalleryItem = event.relatedTarget; // The element that triggered the modal
+        const imageSrc = clickedGalleryItem.getAttribute('data-image-src');
+        modalImage.setAttribute('src', imageSrc);
+    });
+
+    galleryItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const imageSrc = this.getAttribute('data-image-src');
+            modalImage.setAttribute('src', imageSrc);
+
+            imageModalInstance = new bootstrap.Modal(imageModalElement);
+            imageModalInstance.show();
+        });
+    });
+});
+
+
+
